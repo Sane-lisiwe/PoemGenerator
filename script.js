24488852ad9ceac5a00f6aa57de2c3c66b1f7469
Sanelisiwe Khumalo <sanelisiwe1123@gmail.com>
2025-06-14 19:59:55 +0200

    Added API Intergration

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: " The Tomb said to the Rose",
+function displayPoem(response){
+    new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+
+
+
+function generatePoem(event) {
+  event.preventDefault();
+let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "9ate0486673b29597db0of00a07a27ea";
+  let prompt = `User instructions : Generate an English poem about ${instructionInput.value}`;
+  let context = "You are a romantic Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic html.Make sure to follow the user instructions" ;
+  let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+  
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
