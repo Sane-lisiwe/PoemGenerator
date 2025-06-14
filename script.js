6035c80021c3d50937bbef07af24fcbe8d59b52d
Sanelisiwe Khumalo <sanelisiwe1123@gmail.com>
2025-06-14 20:03:28 +0200

    Updated js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
 let instructionInput = document.querySelector("#user-instructions");
   let apiKey = "9ate0486673b29597db0of00a07a27ea";
   let prompt = `User instructions : Generate an English poem about ${instructionInput.value}`;
-  let context = "You are a romantic Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic html.Make sure to follow the user instructions" ;
+  let context = "You are a romantic Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic html.Make sure to follow the user instructions.Please remove the word html and the back apostrophes at the beginning and end of the poem" ;
   let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
