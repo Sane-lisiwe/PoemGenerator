0e5d706c6f34d351a0b73588f6e6ad5ce9524396
Sanelisiwe Khumalo <sanelisiwe1123@gmail.com>
2025-06-14 20:15:03 +0200

    UserExperiences

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ let instructionInput = document.querySelector("#user-instructions");
   let context = "You are a romantic Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic html.Make sure to follow the user instructions.Please remove the word html and the back apostrophes at the beginning and end of the poem" ;
   let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an English poem about ${instructionInput.value}</div>`;
+
   axios.get(apiUrl).then(displayPoem);
   
 }
